dc8509093cabc642e8473e026cf9b95a6e0f97e7
SceLPI <l.eduardosoares@gmail.com>
2025-08-05 18:18:10 -0300

    OrderItemEntity on TypeORM added (missign relationships)

--- src/infrastructure/database/entities/OrderItemEntity.ts
+++ src/infrastructure/database/entities/OrderItemEntity.ts
@@ -1,7 +1,35 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ItemEntity } from './ItemEntity';
 
 @Entity('order_items')
 export class OrderItemEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
+  quantity: number;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  value: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @ManyToOne(() => ItemEntity, {
+    eager: true,
+    nullable: false,
+  })
+  @JoinColumn({ name: 'item_id' })
+  item: ItemEntity;
 }
