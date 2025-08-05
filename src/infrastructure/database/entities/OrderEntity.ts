ed4f28a7d53293f081aa660b48b945f80b3799f8
SceLPI <l.eduardosoares@gmail.com>
2025-08-05 18:16:43 -0300

    Order Entity on TypeORM added (missign relationships)

--- src/infrastructure/database/entities/OrderEntity.ts
+++ src/infrastructure/database/entities/OrderEntity.ts
@@ -1,7 +1,22 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('orders')
 export class OrderEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'varchar' })
+  status: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
