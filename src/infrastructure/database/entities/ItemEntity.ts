d18ccbd9f1feca223e2e782ce8d539c6be5d2de8
SceLPI <l.eduardosoares@gmail.com>
2025-08-05 18:11:30 -0300

    Item columns mapped on TypeORM

--- src/infrastructure/database/entities/ItemEntity.ts
+++ src/infrastructure/database/entities/ItemEntity.ts
@@ -1,7 +1,25 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('item')
 export class ItemEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
